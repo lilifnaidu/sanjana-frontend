79c500cbe6a2965dce27552235694f846840fbf0
Lilif Naidu Indla <postman2489@gmail.com>
2025-11-20 08:15:08 +0530

    api udates

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -34,7 +34,7 @@ export default function LoginForm() {
       await schema.validate(form, { abortEarly: false });
       setErrors({});
 
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('https://server-qkb9j5wh2-lilifs-projects.vercel.app/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form)
